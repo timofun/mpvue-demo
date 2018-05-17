89020772510e3ba2f8b79a8a437bf11384f7191f
timofun <timo_quiet@163.com>
2018-05-17 17:52:09 +0800

    新建demo详情页

---
+++ src/pages/detail/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page">
+    <p>
+      nihao 
+    </p>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+</style>
+
