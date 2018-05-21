a6cd10c6bd7e1360a0ce6fd5ffdb985648a2caa0
timofun <timo_quiet@163.com>
2018-05-21 18:33:20 +0800

    新增个人中心

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script>
-import { get } from './util'
+// import { get } from './util'
 export default {
   async created () {
-    // 同步
-    const res = await get('/weapp/demo')
-    console.log(res)
+    // // 同步
+    // const res = await get('/weapp/demo')
+    // console.log(res)
 
     // 异步
     // get('/weapp/demo').then(res => {
@@ -20,27 +20,33 @@ export default {
     //     console.log(res)
     //   }
     // })
-
-    console.log('app created and cache logs by setStorageSync')
+    console.log('启动小程序')
   }
 }
 </script>
 
 <style>
-.container {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  padding: 200rpx 0;
-  box-sizing: border-box;
+.text-footer{
+  text-align: center;
+  font-size: 12px;
+  margin-bottom:5px;
 }
-/* this rule will be remove */
-* {
-  transition: width 2s;
-  -moz-transition: width 2s;
-  -webkit-transition: width 2s;
-  -o-transition: width 2s;
+.text-primary{
+  color:#EA5149;
+}
+.btn{
+  color:white;
+  background:#EA5A49;
+  margin-bottom: 10px;
+  padding-left: 15px;
+  padding-left: 15px;
+  border-radius: 2px;
+  font-size: 16px;
+  line-height: 40px;
+  height: 40px;
+  width: 100%;
+}
+.btn:active{
+  background: #FA5A49;
 }
 </style>
--- src/pages/me/Me.vue
+++ src/pages/me/Me.vue
@@ -1,21 +1,91 @@
 <template>
-  <div class="me-page">
-    个人中心
+  <div class="container">
+    <div class="userinfo" @click='login'>
+      <img :src="userinfo.avatarUrl" alt="">
+      <p>{{userinfo.nickName}}</p>
+    </div>
+    <YearProgress></YearProgress>
+
+    <!-- <button open-type="getUserInfo" @click='login' class='btn'></button>  -->
+
+    <button v-if='userinfo.openId' @click='scanBook' class='btn'>添加图书</button>
   </div>
 </template>
-
-<script type="text/ecmascript-6">
+<script>
+import qcloud from 'wafer2-client-sdk'
+import YearProgress from '@/components/YearProgress'
+import { showSuccess } from '@/util'
+import config from '@/config'
 export default {
+  components: {
+    YearProgress
+  },
   data () {
     return {
-
+      userinfo: {
+        avatarUrl: '../../../static/img/default-avatar.png',
+        nickName: '点击登录'
+      }
     }
   },
-  components: {
-
+  methods: {
+    scanBook () {
+      wx.scanCode({
+        success: (res) => {
+          if (res.result) {
+            console.log(res.result)
+          }
+        }
+      })
+    },
+    login () {
+      let user = wx.getStorageSync('userinfo')
+      const self = this
+      if (!user) {
+        qcloud.setLoginUrl(config.loginUrl)
+        qcloud.login({
+          success: function (userinfo) {
+            qcloud.request({
+              url: config.userUrl,
+              login: true,
+              success (userRes) {
+                showSuccess('登录成功')
+                wx.setStorageSync('userinfo', userRes.data.data)
+                self.userinfo = userRes.data.data
+              }
+            })
+          },
+          fail: function (err) {
+            console.log('登录失败', err)
+          }
+        })
+      }
+    }
+  },
+  onShow () {
+    let userinfo = wx.getStorageSync('userinfo')
+    if (userinfo) {
+      this.userinfo = userinfo
+    }
   }
 }
 </script>
 
-<style scoped>
+<style lang='scss'>
+.container{
+  padding:0 30rpx;
+
+}  
+.userinfo{
+  margin-top:100rpx;
+  text-align:center;
+  img{
+    width: 150rpx;
+    height:150rpx;
+    margin: 20rpx;
+    border-radius: 50%;
+  }
+}
+
+
 </style>
